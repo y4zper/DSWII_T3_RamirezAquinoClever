3a40f8c8dcfa1d2d244ec829271dc715704a484b
y4zper <ander.crr@gmail.com>
2023-11-25 22:23:02 -0500

    e

--- src/main/java/com/CL3RamirezAquinoClever/ExamenCL3/controller/FileController.java
+++ src/main/java/com/CL3RamirezAquinoClever/ExamenCL3/controller/FileController.java
@@ -5,28 +5,51 @@ import com.CL3RamirezAquinoClever.ExamenCL3.service.FileService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-
-import java.util.List;
-
-@AllArgsConstructor
+import java.io.IOException;
 @RestController
-@RequestMapping("api/v1/file")
+@CrossOrigin(origins = "https://www.cibertec.edu.pe")
 public class FileController {
-    private FileService fileService;
-    @PostMapping("/upload")
-    public ResponseEntity<ResponseFile> subirArchivos(
-            @RequestParam("files")List<MultipartFile> files) throws  Exception{
-        fileService.guardarArchivos(files);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(
-                        ResponseFile.builder().message("Los archivos fueron cargados correctamente")
-                                .build()
-                );
+    private static final String IMAGES_DIRECTORY = "images";
+    private static final String DOCUMENTS_DIRECTORY = "documents";
+    private static final long MAX_FILE_SIZE = 1_500_000; // 1.5MB
+
+    @PostMapping("/filesimages")
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        validateFileExtension(file, "png");
+        return saveFile(file, IMAGES_DIRECTORY);
+    }
+
+    @PostMapping("/filesexcel")
+    public ResponseEntity<String> uploadExcelFile(@RequestParam("file") MultipartFile file) throws IOException {
+        validateFileExtension(file, "xlsx");
+        validateFileSize(file, MAX_FILE_SIZE);
+        return saveFile(file, DOCUMENTS_DIRECTORY);
+    }
+
+    private ResponseEntity<String> saveFile(MultipartFile file, String directory) throws IOException {
+
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/download/")
+                .path(file.getOriginalFilename())
+                .toUriString();
+
+        return ResponseEntity.ok("Archivo subido correctamente. URL de descarga: " + fileDownloadUri);
+    }
+
+    private void validateFileExtension(MultipartFile file, String allowedExtension) {
+        if (!file.getOriginalFilename().toLowerCase().endsWith(allowedExtension)) {
+            throw new IllegalArgumentException("Extensión no permitida");
+        }
+    }
+
+    private void validateFileSize(MultipartFile file, long maxSize) {
+        if (file.getSize() > maxSize) {
+            throw new IllegalArgumentException("Tamaño no permitido");
+        }
     }
 }
---
+++ src/main/java/com/CL3RamirezAquinoClever/ExamenCL3/security/Cors.java
@@ -0,0 +1,26 @@
+package com.CL3RamirezAquinoClever.ExamenCL3.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+
+public class Cors {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://www.cibertec.edu.pe")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
